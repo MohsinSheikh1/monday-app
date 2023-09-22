0dcf726240cc23bfa3326af0f2c088609e6f63b7
faique43 <fkhan.bese20seecs@seecs.edu.pk>
2023-09-23 02:12:11 +0500

    madarchod

--- src/Components/Export.jsx
+++ src/Components/Export.jsx
@@ -1,77 +1,117 @@
-import React from "react";
-
-import axios from "axios";
-
+import { useState } from "react";
 import { Checkbox } from "monday-ui-react-core";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 const Export = ({ monday, context }) => {
+  const [data, setData] = useState("");
+  const [includeUpdates, setIncludeUpdates] = useState(false);
+  const [includeSubItems, setIncludeSubItems] = useState(false);
+  const [sendCopyToEmail, setSendCopyToEmail] = useState(false);
+
+  const generatePDF = () => {
+    // Create a new jsPDF instance
+    const doc = new jsPDF();
+
+    // Convert table data to HTML based on checkboxes
+    const tableHtml = `<table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Item</th>
+          <th>Person</th>
+          <th>Status</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${parseDataForTable()}
+      </tbody>
+    </table>`;
+
+    // Use html2canvas to convert the table to an image
+    html2canvas(document.querySelector("#pdfTable")).then((canvas) => {
+      const imgData = canvas.toDataURL("./");
+      doc.addImage(imgData, "PNG", 10, 10, 180, 0);
+      doc.save("table.pdf");
+    });
+  };
+
+  const parseDataForTable = () => {
+    // Implement your logic to parse data based on checkboxes
+    // Return the HTML table rows accordingly
+    // You can use the data state variable for this
+
+    return "";
+  };
+
   return (
-    <div className="flex flex-col items-start justify-between h-[50%] w-[50%]">
-      <h1 className="font-bold text-4xl text-white">Export to PDF</h1>
-      <div>
-        <Checkbox CheckboxClassName="text-white" label="Include Updates" />
-        <Checkbox CheckboxClassName="text-white" label="Include SubItems" />
-        <Checkbox
-          labelClassName="primary-text-color"
-          label="Send a copy to my email"
-        />
-      </div>
+    <>
+      <div className="flex flex-col items-start justify-between h-[50%] w-[50%]">
+        <h1 className="font-bold text-4xl text-white">Export to PDF</h1>
+        <div>
+          <Checkbox
+            CheckboxClassName="text-white"
+            label="Include Updates"
+            onChange={() => setIncludeUpdates(!includeUpdates)}
+          />
+          <Checkbox
+            CheckboxClassName="text-white"
+            label="Include SubItems"
+            onChange={() => setIncludeSubItems(!includeSubItems)}
+          />
+          <Checkbox
+            labelClassName="primary-text-color"
+            label="Send a copy to my email"
+            onChange={() => setSendCopyToEmail(!sendCopyToEmail)}
+          />
+        </div>
 
-      <button
-        className="bg-blue-500 px-4 py-2 rounded-lg hover:text-blue-500 hover:bg-transparent border-2 border-blue-500 box-border"
-        onClick={
-          //   async () => {
-          //   await axios
-          //     .post("https://api.monday.com/v2", {
-          //       method: "post",
-          //       headers: {
-          //         "Content-Type": "application/json",
-          //         Authorization:
-          //           "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI4MTk4Mjg4NywiYWFpIjoxMSwidWlkIjo0ODU5NTMzMiwiaWFkIjoiMjAyMy0wOS0xNFQyMTo0MDo0MS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTg3MTUzNzYsInJnbiI6ImV1YzEifQ.pmVheIJ_ordb6DX7Zzj3_5ztoe7tWM3dMax0nmo-DTM",
-          //         "API-Version": "2023-04",
-          //       },
-          //       body: JSON.stringify({
-          //         query: "query { boards { columns {id title type} }}",
-          //       }),
-          //     })
-          //     .then((res) => console.log(res));
-          // }
-          () => {
-            // monday.api(`query { boards { columns {id title type} }}`).then((res) => console.log(res));
-            console.log(context);
-
+        <button
+          className="bg-blue-500 px-4 py-2 rounded-lg hover:text-blue-500 hover:bg-transparent border-2 border-blue-500 box-border"
+          onClick={() => {
             monday
               .api(
-                `
-query {
-  boards(ids: [${context.boardId}]) {
-    name
-    groups(ids: ["${context.groupId}"]) {
-    	items {
-        id
-        name
-        column_values {
-          value
-          type
-          id
-        }
-      }
-    }
-  }
-}
-             `,
+                `query {
+                boards(ids: [${context.boardId}]) {
+                  name
+                  groups(ids: ["${context.groupId}"]) {
+                    items {
+                      id
+                      name
+                      column_values {
+                        value
+                        type
+                        id
+                      }
+                    }
+                  }
+                }
+              }`,
                 {
                   token:
-                    "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI4MTk4Mjg4NywiYWFpIjoxMSwidWlkIjo0ODU5NTMzMiwiaWFkIjoiMjAyMy0wOS0xNFQyMTo0MDo0MS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTg3MTUzNzYsInJnbiI6ImV1YzEifQ.pmVheIJ_ordb6DX7Zzj3_5ztoe7tWM3dMax0nmo-DTM",
+                    "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjI4MTk4Mjg4NywiYWFpIjoxMSwidWlkIjo0ODU5NTMzMiwiaWFkIjoiMjAyMy0wOS0xNFQyMTo0MDo0MS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTg3MTUzNzYsInJnbiI6ImV1YzEifQ.pmVheIJ_ordb6DX7Zzj3_5ztoe7tWM3dMax0nmo-DTM"
                 }
               )
-              .then((res) => console.log(res));
-          }
-        }
-      >
-        Export
-      </button>
-    </div>
+              .then((res) => {
+                setData(JSON.stringify(res.data.boards));
+                generatePDF();
+              })
+              .catch((error) => {
+                console.error("API call error:", error);
+                // Handle error here
+              });
+          }}
+        >
+          Export
+        </button>
+        <div
+          id="pdfTable"
+          dangerouslySetInnerHTML={{ __html: data }}
+          style={{ display: "none" }}
+        />
+      </div>
+    </>
   );
 };
 
