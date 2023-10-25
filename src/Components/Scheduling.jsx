420cdd0e109de9d4f2a56c49a53c62edb2c2450a
faique43 <fkhan.bese20seecs@seecs.edu.pk>
2023-10-26 01:29:35 +0500

    button position

--- src/Components/Scheduling.jsx
+++ src/Components/Scheduling.jsx
@@ -115,13 +115,13 @@ const Scheduling = () => {
       )}
       {!loading && (
         <>
+          <button
+            onClick={() => navigate("/export")}
+            className="text-white px-4 py-2 bg-blue-500 rounded-lg"
+          >
+            Back
+          </button>
           <div className="flex flex-row items-center justify-start w-full gap-2">
-            <button
-              onClick={() => navigate("/export")}
-              className="text-white px-4 py-2 bg-blue-500 rounded-lg"
-            >
-              Back
-            </button>
             <h1 className="text-white font-bold text-4xl">
               Schedule the PDF Export for Later
             </h1>
